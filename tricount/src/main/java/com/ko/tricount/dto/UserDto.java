2833d298c4090eee78eac3a65d0060fc174bd9cf
ko <iamkokoko123@gmail.com>
2024-01-24 00:35:55 +0900

    feat: get all users & users' participation

---
+++ tricount/src/main/java/com/ko/tricount/dto/UserDto.java
@@ -0,0 +1,21 @@
+package com.ko.tricount.dto;
+
+import com.ko.tricount.entity.SettlementParticipant;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+@Data
+@AllArgsConstructor
+public class UserDto {
+
+    private Long id;
+    private String loginId;
+    private String password;
+    private String nickname;
+
+    private List<Long> settlements;
+}
