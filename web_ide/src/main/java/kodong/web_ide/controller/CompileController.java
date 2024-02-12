a83ea7acdf69d5ed342290cfce76fb175881fd42
ko <iamkokoko123@gmail.com>
2024-02-12 22:18:53 +0900

    finish

--- web_ide/src/main/java/kodong/web_ide/controller/CompileController.java
+++ web_ide/src/main/java/kodong/web_ide/controller/CompileController.java
@@ -4,6 +4,8 @@ package kodong.web_ide.controller;
 import kodong.web_ide.builder.CompileBuilder;
 import kodong.web_ide.model.dto.RunResult;
 import kodong.web_ide.model.dto.RunResultDto;
+import kodong.web_ide.model.dto.SubmitResult;
+import kodong.web_ide.model.dto.SubmitResultDto;
 import kodong.web_ide.service.TestCaseService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -15,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 
@@ -31,4 +34,11 @@ public class CompileController {
         return ResponseEntity.ok(new RunResultDto(result));
     }
 
+    // todo 제출 -> 공개테케 + 히든테케 + 테스트결과 + 채점 결과
+    @PostMapping("/{problemId}/submit")
+    public ResponseEntity<SubmitResultDto> submit(@RequestBody Map<String, Object> input, @PathVariable(name = "problemId") Long problemId) throws Exception {
+        ArrayList<RunResult> result = testCaseService.compileAndRun(problemId, input.get("code").toString());
+        List<SubmitResult> submits = testCaseService.submit(problemId, input.get("code").toString());
+        return ResponseEntity.ok(new SubmitResultDto(result, submits));
+    }
 }
